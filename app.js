3b208b32c4274d61e7c1708f3d38619164044f9e
Stone <qstandsforquail@gmail.com>
2022-09-14 15:18:53 -0400

    make media query for desktop

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 let nameInput = document.querySelector('#name')
 let nameOnCard = document.querySelector('.name-on-card')
+let numberInput = document.querySelector('#number')
+let numberOnCard = document.querySelector('.card-number')
+
 
 nameInput.addEventListener('input', () => {
   nameOnCard.textContent = nameInput.value
 })
 
+// numberInput.addEventListener('input', () => {
+//   numberOnCard.textContent = numberInput.value
+//   if(numberInput.value.length > numberInput.maxLength) {
+//     alert('too many numbers')
+//     return
+//   }
+// })
+
+numberInput.addEventListener('input', () => {
+  if(numberInput.value.length > numberInput.maxLength) {
+    return
+  } else {
+    
+  }
+})
